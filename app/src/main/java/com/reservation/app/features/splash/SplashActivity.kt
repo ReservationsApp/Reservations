23043652a6e2d3c1007e05bf6d6499a19735d944
Khalid Elnagar <khalid.3lnagar@gmail.com>
2019-09-18 00:17:33 +0200

    add: splash activity

--- app/src/main/java/com/reservation/app/features/splash/SplashActivity.kt
+++ app/src/main/java/com/reservation/app/features/splash/SplashActivity.kt
@@ -1,13 +1,13 @@
-package com.reservation.app.features
+package com.reservation.app.features.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.reservation.app.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
